30b09dfa575ba28d75ef8a2db8b425cf36971a33
dehong <dehong.lecture@gmail.com>
2019-08-05 17:18:34 +0900

    1.login 프로세스 개발   - jsp to jsp --> servlet to jsp 2.bootstrap 적용   - login.jsp   - main.jsp 3.웹 어플리케이션 scope   - application   - session(*)   - request(*)   - pageContext   -- attribute : 공유할 데이터   -- servlet에서 처리한 데이터를 jsp에서 사용할 수 있게끔 공유

---
+++ src/main/java/kr/or/ddit/login/web/LoginController.java
@@ -0,0 +1,81 @@
+package kr.or.ddit.login.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kr.or.ddit.user.model.UserVo;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+       
+	/**
+	 * 
+	* Method : doGet
+	* 작성자 : PC-04
+	* 변경이력 :
+	* @param request
+	* @param response
+	* @throws ServletException
+	* @throws IOException
+	* Method 설명 : 로그인 화면 요청 처리
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// webapp/jsp/login.jsp --> url 형태로 바꾸면?? /jsp/login.jsp
+		request.getRequestDispatcher("/login/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * 
+	* Method : doPost
+	* 작성자 : PC-04
+	* 변경이력 :
+	* @param request
+	* @param response
+	* @throws ServletException
+	* @throws IOException
+	* Method 설명 : 로그인 요청 처리
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.debug("login controller doPost()");
+		
+		// userId, password 파라미터 logger 출력
+		String userId = request.getParameter("userId");
+		String pass = request.getParameter("pass");
+		
+		logger.debug("userId : {}", userId);
+		logger.debug("password : {}", pass);
+		
+		// 사용자가 입력한 계정정보와 db에 있는 값이랑 비교
+		UserVo userVo = new UserVo();
+		userVo.setUserName("브라운");
+		userVo.setUserId("brown");
+		userVo.setPass("brown1234");
+		
+		// 사용자가 입력한 파라미터 정보와 db에서 조회해온 값이 동일할 경우 --> webapp/main.jsp
+		// 사용자가 입력한 파라미터 정보와 db에서 조회해온 값이 다를 경우 --> webapp/login/login.jsp
+		if(userId.equals(userVo.getUserId()) && pass.equals(userVo.getPass())) {
+			
+			HttpSession session = request.getSession();
+			logger.debug("session.getId() : {}", session.getId());
+			
+			session.setAttribute("S_USERVO", userVo);
+			
+			request.getRequestDispatcher("/main.jsp").forward(request, response);
+		}else {
+			//request.getRequestDispatcher("/login/login.jsp").forward(request, response);
+			//request.setAttribute("userId", userId);
+			doGet(request, response);
+		}
+	}
+}
--- src/main/java/kr/or/ddit/user/model/UserVo.java
+++ src/main/java/kr/or/ddit/user/model/UserVo.java
@@ -2,21 +2,40 @@ package kr.or.ddit.user.model;
 
 public class UserVo {
 	private String userName;		// 사용자 이름
+	private String userId;			// 사용자 아이디
+	private String pass;			// 사용자 비밀번호
 
-	public UserVo(String userName) { // alt + shift + R 이름 동시에 바꾸기
-		this.userName = userName;
+	public UserVo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	 public UserVo(String userName) { // alt + shift + R 이름 동시에 바꾸기 
+		 this.userName = userName; }
+	
+	 public String getUserName() { return userName; }
+	
+	 public void setUserName(String userName) { this.userName = userName; }
+	
+
+	public String getUserId() {
+		return userId;
 	}
 
-	public String getUserName() {
-		return userName;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
 	}
 
 	@Override
 	public String toString() {
-		return "UserVo [userName=" + userName + "]";
+		return "UserVo [userName=" + userName + ", userId=" + userId + ", pass=" + pass + "]";
 	}
+
 }
