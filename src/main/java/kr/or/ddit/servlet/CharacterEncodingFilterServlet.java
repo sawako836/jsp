51ed1859cd211170e509dea34daf7d8ae33f03d0
dehong <dehong.lecture@gmail.com>
2019-09-05 15:19:17 +0900

    "파라미터 임의로 수정하기  HttpServletRequestWrapper 상속   . parameterMap request객체의 파라미터 맵을 바탕으로 신규생성   . 파라미터 관련 메소드 4가지를 재정의     . getParameter     . getParameterValues     . getParameterMap     . getParameterNames   . filter에서 메소드 인자로 받은 request 객체 대신에      wrapper 인스턴스를 생성하여 doFilter 메소드에 인자로 전달(request -> wrapper)
    CharacterEncodigFilter 적용
     . servlet에 있는 requeset.setCharacterEncoding("utf-8") 호출 부분을
        filter로 이관, 코드를 한곳에서만 호출되도록 수정"

---
+++ src/main/java/kr/or/ddit/servlet/CharacterEncodingFilterServlet.java
@@ -0,0 +1,23 @@
+package kr.or.ddit.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CharacterEncodingFilterServlet")
+public class CharacterEncodingFilterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
